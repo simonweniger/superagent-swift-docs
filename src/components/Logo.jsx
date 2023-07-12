e5e80d8db337fa5cc8b9c085546ed4c653d5a4b4
Simon Weniger <simonweniger.sw@gmail.com>
2023-07-12 18:28:58 +0200

    superagent swift docs init

---
+++ src/components/Logo.jsx
@@ -0,0 +1,7 @@
+export function Logo(props) {
+  return (
+    <div className="inline-block text-xl sm:text-2xl font-extrabold text-gray-900 tracking-tight dark:text-gray-200 w-auto h-7 relative">
+    Superagent Swift
+    </div>
+  )
+}
